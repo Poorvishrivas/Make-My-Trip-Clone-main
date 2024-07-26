738a18dc83a69643a1d4e8dba33617b8621323d9
Poorvi Shrivas <poorvishrivas@gmail.com>
2024-07-27 02:38:01 +0530

    change

--- BACKEND/config/config.js
+++ BACKEND/config/config.js
@@ -1,4 +1,4 @@
 module.exports = {
   jwtSecret: process.env.JWT_SECRET || "MyScretKey",
-  mongoURI: process.env.MONGODB_URI || "mongodb://0.0.0.0:27017/poorvi",
+  mongoURI: process.env.MONGODB_URI || "mongodb://0.0.0.0:27017/MakeMyTripDB",
 };
