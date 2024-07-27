e5c55bf58052c4ecee6cd1a2b79831176e9d285e
Poorvi Shrivas <poorvishrivas@gmail.com>
2024-07-28 03:33:01 +0530

    '

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -17,7 +17,10 @@ app.use(express.json({ extended: false }));
 //app.use(cors());
 app.use(
   cors({
-    origin: "https://make-my-trip-clone-main-frontend.vercel.app", // Replace with your React app's domain
+    origin: [
+      "https://make-my-trip-clone-main-frontend.vercel.app",
+      "https://make-my-trip-clone-main-frontend-c9udwe7oa.vercel.app", // Add additional origins as needed
+    ],
     credentials: true, // Allow cookies for authenticated requests (if applicable)
   })
 );
